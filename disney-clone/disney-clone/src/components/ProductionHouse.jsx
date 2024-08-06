95d777734572aae3f2eb33f4a54f875ec8e122b6
Berkay OCER <berkayocer2003@gmail.com>
2024-08-06 09:10:24 +0300

    update

--- disney-clone/disney-clone/src/components/ProductionHouse.jsx
+++ disney-clone/disney-clone/src/components/ProductionHouse.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import disney from "../assets/images/disney.png";
 import marvel from "../assets/images/marvel.png";
 import nationalG from "../assets/images/nationalG.png";
@@ -39,52 +39,35 @@ function ProductionHouse() {
       video: starwarsV,
     },
   ];
-
-  const handleMouseOver = (video) => {
-    video.play();
-  };
-
-  const handleMouseOut = (video) => {
-    video.pause();
-  };
-
   return (
     <div className="flex md:mx-10 gap-5 md:gap-10 p-0 md:p-2 px-5 md:px-16 relative">
       {ProductionHouseList.map((item) => (
         <div
-          key={item.id}
-          className="
-          border-[2px]
-          border-gray-600
-          rounded-lg
-          hover:scale-105
-          transition-all 
-          w-screen
-          md:w-full
-          ease-in-out 
-          cursor-pointer
-          relative
-          shadow-2xl
-          shadow-gray-700"
+        className="
+        border-[2px]
+        border-gray-600
+        rounded-lg
+        hover:scale-105
+        transition-all 
+        w-screen
+        md:w-full
+        ease-in-out 
+        cursor-pointer
+        relative
+        shadow-2xl
+        shadow-gray-700"
         >
           <div>
             <video
               src={item.video}
               loop
+              autoPlay
               playsInline
-              className="absolute rounded-md opacity-0 hover:opacity-50"
+              className="absolute rounded-md hover:opacity-50
+             opacity-0"
             />
           </div>
-          <img
-            src={item.image}
-            className="w-full top-0"
-            onMouseOver={(e) =>
-              handleMouseOver(e.target.parentNode.querySelector("video"))
-            }
-            onMouseOut={(e) =>
-              handleMouseOut(e.target.parentNode.querySelector("video"))
-            }
-          />
+          <img src={item.image} className="w-full top-0"/>
         </div>
       ))}
     </div>
